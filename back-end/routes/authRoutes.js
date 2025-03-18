3c1fcf725226265457e2057770c6b03440590590
liu301318128 <qliu103@my.centennialcollege.ca>
2025-03-18 17:53:54 -0400

    Update authRoutes.js

--- back-end/routes/authRoutes.js
+++ back-end/routes/authRoutes.js
@@ -1,1 +1,10 @@
- 
+const express = require("express");
+const { loginUser, registerUser } = require("../controllers/authController");
+const router = express.Router();
+
+router.post("/signup", registerUser);
+
+router.post("/login", loginUser);
+
+module.exports = router;
+
